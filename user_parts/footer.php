6610f296b9cf8a8fc06fac52187bbcd3beab52be
Valeze Vranovci <valeze.vranovci@gmail.com>
2020-02-11 14:10:33 +0100

    Create footer.php

---
+++ user_parts/footer.php
@@ -0,0 +1,47 @@
+<div class="margin-top"></div>
+
+
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <p><span class="footer-span">Ndihme:</span></p>
+                    <ul>
+                        <li><a href="rreth_nesh.html">Kontaktoni</a></li>
+                        <li><a href="rreth_nesh.html">Raportoni</a></li>
+                    </ul> 
+                </div>
+                <div class="col-md-4">
+                    <p><span class="footer-span">Rreth nesh:</span></p>
+                    <ul>
+                        <li><a href="rreth_nesh.html">Kush jemi ne?</a></li>
+                    </ul> 
+                </div>
+                <div class="col-md-4">
+                    <p><span class="footer-span">Na ndicni ne:</span></p>
+                    <ul>
+                        <li><a href="www.facebook.com">Facebook</a></li>
+                        <li><a href="www.facebook.com">Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>&copy; FoodMania @ 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+<script src="bootstrap/js/bootstrap.min.js" ></script>
+
+</body>
+</html>
